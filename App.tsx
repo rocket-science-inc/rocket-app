b9073787b7ade95ee83ebe5c82fe0e71a5927ccd
Andrey Majskiy <majskiy.andrey@gmail.com>
2019-01-23 23:21:38 +0200

    Migration to TypeScript

--- App.tsx
+++ App.tsx
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,7 +9,8 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+
+export class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
